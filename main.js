6da016ebe1a64486b1e666ec06ba25ae915fdabf
Gracjan Kilanczyk <Gracjan.Kilanczyk@outlook.de>
2023-11-11 09:59:45 +0100

    // jquery

--- main.js
+++ main.js
@@ -1,17 +1,8 @@
-// array fulter() & map()
+// jquery
 
 function main(){
 
-    // filter
-let numbers = [1,5,3,29,83,10,8,4]
-
-let smallNumbers = numbers.filter(n =>{
-    return n < 10
-});
-
-console.log(smallNumbers)
-
-// map()
-let doubledNumbers = numbers.map(n => n* 2);
-console.log(doubledNumbers)
+// $("div").toggle(); für komplette Tags
+$("#top").toggle();
+$("#sub").toggle();
 }
