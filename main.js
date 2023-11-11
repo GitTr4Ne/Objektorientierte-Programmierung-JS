4a69970ff92718f0366a98477da932366b77bb9c
Gracjan Kilanczyk <Gracjan.Kilanczyk@outlook.de>
2023-11-11 09:14:04 +0100

    // Promises

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-// Try Catch
+// Promises
 
 function main(){
 
-try {
+let promise = new Promise((resolve, reject) => {
 
-    var n = 600;
+    let n = 99;
 
-    if (n > 0 && n < 1000){
-        console.log("Die nummer ist zwischen 0 und 1000")
+    if(n < 100) {
+        resolve("Versprechen gehalten")
     } else {
-        throw new RangeError("die Nummer ist zu groß oder zu klein")
+        reject("versprechen nicht gehalten")
     }
-    console.log(age)
-} catch(e){
-    console.log("Error abgefangen: " + e)
+})
 
-    if(e instanceof ReferenceError)
-    console.log("Referenz fehler")
-
-}finally
-{
-    //wird in jedem fall ausgeführt
-    console.log("Ich mag kuchen")
-}
-    console.log(10/0)
-    console.log("Hello World")
-
+    promise.then((res) => {
+        console.log(res)
+    }).catch((e) => {
+        console.log("Error: "+ e)
+    })
 }
